85ad44e07cf5a47c8a9eb59e39a6f8de2b9aed22
Gaurav Kushwaha <gaurav.family04@gmail.com>
2024-02-13 15:10:47 +0530

    Landscape mode disabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:aeroaura/utils/routes.dart';
 import 'package:aeroaura/utils/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const AeroAura());
@@ -10,6 +11,9 @@ class AeroAura extends StatelessWidget {
   const AeroAura({super.key});
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp
+      ]);
     return MaterialApp.router(
       themeMode: ThemeMode.system,
       theme: AeroTheme.lightTheme,
