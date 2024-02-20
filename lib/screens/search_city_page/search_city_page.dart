3a8c8dae0f239563df7b501f29524f615e72f387
Gaurav Kushwaha <gaurav.family04@gmail.com>
2024-02-20 22:13:50 +0530

    Updates

--- lib/screens/search_city_page/search_city_page.dart
+++ lib/screens/search_city_page/search_city_page.dart
@@ -1,6 +1,8 @@
 import 'package:aeroaura/screens/search_city_page/local_widgets/app_bar.dart';
+import 'package:aeroaura/utils/consts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SearchCityPage extends StatefulWidget {
   const SearchCityPage({super.key});
@@ -11,6 +13,20 @@ class SearchCityPage extends StatefulWidget {
 
 class _SearchCityPageState extends State<SearchCityPage> {
   final TextEditingController searchController = TextEditingController();
+  List<String> city = Constants.cities;
+
+  void searchCity(String query) {
+    final suggestions = Constants.cities.where((element) {
+      final cityName = element.toLowerCase();
+      final input = query.toLowerCase();
+
+      return cityName.contains(input);
+    }).toList();
+    setState(() {
+      city = suggestions;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
@@ -33,8 +49,19 @@ class _SearchCityPageState extends State<SearchCityPage> {
                         padding: EdgeInsets.only(left: 8, right: 5),
                         child: Icon(CupertinoIcons.search),
                       ),
+                      onChanged: searchCity,
                     ),
-                  )
+                  ),
+                  SizedBox(),
+                  Expanded(
+                      child: ListView.builder(
+                    itemCount: city.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(city[index]),
+                      );
+                    },
+                  ))
                 ]));
   }
 }
